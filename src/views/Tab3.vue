523f40a05e699df1bb1a123b0e0bf04ca586d49f
Plasmarocket <59854769+Plasmarocket@users.noreply.github.com>
2021-02-15 20:03:27 -0500

    Adding new to tab3
    Adding components to profile page

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -1,29 +1,37 @@
 <template>
+<link rel="stylesheet" href="./main.css">
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>My Account</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+        
+          <ion-title size="large">Profile</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <ion-card-content>
+        <img class="profile photo" height="100" width="100" src="../Images/Default.png" />
+        <h2>UserName</h2>
       
-      <ExploreContainer name="Profile Page or whatever" />
+      </ion-card-content>
+      
     </ion-content>
+    
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 
-import ExploreContainer from '@/components/ExploreContainer.vue';
+
 
 export default  {
-  name: 'Tab3',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: 'Profile',
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 }
 </script>
