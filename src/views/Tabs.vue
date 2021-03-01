a570b800dfb9f5ee91140f39ffeba577f51ea76f
Pteropticonn <ParadoxMeltdown@protonmail.com>
2021-03-01 10:24:19 -0500

    Fixed issue where "feed" tab button rendered on a new row instead of adjacent to tab 3 button on the same row

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,5 +1,4 @@
 import { images, triangle } from 'ionicons/icons';
-
 <template>
   <ion-page>
     <ion-tabs>
@@ -8,30 +7,30 @@ import { images, triangle } from 'ionicons/icons';
           <ion-icon :icon="triangle" />
           <ion-label>Explore</ion-label>
         </ion-tab-button>
-          
-<ion-tab-button tab="tab2" href="/tabs/tab2">
-  <ion-icon :icon="images" />
-  <ion-label>Photos</ion-label>
-</ion-tab-button>
-        
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="images" />
+          <ion-label>Photos</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab3" href="/tabs/feed">
           <img class="profile photo" height="22" width="22" src="../Images/Default.png" />
           <ion-label>Profile</ion-label>
         </ion-tab-button>
-      </ion-tab-bar>
-       <ion-tab-bar @slot="bottom">
+
         <ion-tab-button tab="feed" href="/tabs/feed">
-          <ion-icon :icon="images" />
+          <ion-icon :icon="colorFilterOutline" />
           <ion-label>Feed</ion-label>
         </ion-tab-button>
-       </ion-tab-bar>
+      </ion-tab-bar>
+
     </ion-tabs>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import {images, triangle } from 'ionicons/icons';
+import {images, triangle, colorFilterOutline } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
@@ -40,6 +39,7 @@ export default {
     return {
       images,  
       triangle,
+      colorFilterOutline
     }
   }
 }
