e1c1360348cadadba9b6c8c3acadd5ed3ac7adb7
bsamu <bsamu@DESKTOP-QKVKKU1>
2021-04-19 23:17:38 -0400

    changed super-base.config

--- src/supabase-config.js
+++ src/supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjE1ODk1MjU5LCJleHAiOjE5MzE0NzEyNTl9.HYsQ2yMxwrIYKzQAAsNlmxxAjeAJr1kSUQK40vRW6MQ";
-const SUPABASE_URL = "https://ssggftqivpapiiblyect.supabase.co/";
+const SUPABASE_URL = "https://ssggftqivpapiiblyect.supabase.co";
 
 let SUPABASE_CLIENT = null;
 
