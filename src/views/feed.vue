6861f8b5faa348b8bc1d678dbad765a86b1a57b6
bsamu <bsamu@DESKTOP-QKVKKU1>
2021-04-19 23:48:31 -0400

    Chnaged the grid view in Feed.js with views.

--- src/views/feed.vue
+++ src/views/feed.vue
@@ -1,181 +1,27 @@
 <template>
-  <ion-grid>
-    <ion-row>
-      <ion-col>
-        ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col size="6">
-        ion-col [size="6"]
-      </ion-col>
-      <ion-col>
-        ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col size="3">
-        ion-col [size="3"]
-      </ion-col>
-      <ion-col>
-        ion-col
-      </ion-col>
-      <ion-col size="3">
-        ion-col [size="3"]
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col size="3">
-        ion-col [size="3"]
-      </ion-col>
-      <ion-col size="3" offset="3">
-        ion-col [size="3"] [offset="3"]
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col>
-        ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-        <br>#
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-        <br>#
-        <br>#
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col class="ion-align-self-start">
-        ion-col [start]
-      </ion-col>
-      <ion-col class="ion-align-self-center">
-        ion-col [center]
-      </ion-col>
-      <ion-col class="ion-align-self-end">
-        ion-col [end]
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-        <br>#
-      </ion-col>
-    </ion-row>
-
-    <ion-row class="ion-align-items-start">
-      <ion-col>
-        [start] ion-col
-      </ion-col>
-      <ion-col>
-        [start] ion-col
-      </ion-col>
-      <ion-col class="ion-align-self-end">
-        [start] ion-col [end]
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-        <br>#
-      </ion-col>
-    </ion-row>
-
-    <ion-row class="ion-align-items-center">
-      <ion-col>
-        [center] ion-col
-      </ion-col>
-      <ion-col>
-        [center] ion-col
-      </ion-col>
-      <ion-col>
-        [center] ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-        <br>#
-      </ion-col>
-    </ion-row>
-
-    <ion-row class="ion-align-items-end">
-      <ion-col>
-        [end] ion-col
-      </ion-col>
-      <ion-col class="ion-align-self-start">
-        [end] ion-col [start]
-      </ion-col>
-      <ion-col>
-        [end] ion-col
-      </ion-col>
-      <ion-col>
-        ion-col
-        <br>#
-        <br>#
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col size="12" size-sm>
-        ion-col [size="12"] [size-sm]
-      </ion-col>
-      <ion-col size="12" size-sm>
-        ion-col [size="12"] [size-sm]
-      </ion-col>
-      <ion-col size="12" size-sm>
-        ion-col [size="12"] [size-sm]
-      </ion-col>
-      <ion-col size="12" size-sm>
-        ion-col [size="12"] [size-sm]
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col size="12" size-md>
-        ion-col [size="12"] [size-md]
-      </ion-col>
-      <ion-col size="12" size-md>
-        ion-col [size="12"] [size-md]
-      </ion-col>
-      <ion-col size="12" size-md>
-        ion-col [size="12"] [size-md]
-      </ion-col>
-      <ion-col size="12" size-md>
-        ion-col [size="12"] [size-md]
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col size="6" size-lg offset="3">
-        ion-col [size="6"] [size-lg] [offset="3"]
-      </ion-col>
-      <ion-col size="3" size-lg>
-        ion-col [size="3"] [size-lg]
-      </ion-col>
-    </ion-row>
-  </ion-grid>
+    <ion-header>
+            <clipped-header></clipped-header>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-grid>
+                <ion-row>
+                    <ion-col size="8">
+                        <ion-card class="ion-activated" id="vid-detail">
+                            <ion-card-header>
+                                <ion-card-subtitle>Clipped</ion-card-subtitle>
+                                <ion-card-title>Epic Video, yeah!</ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col>
+                        <static-list-ex></static-list-ex>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <!-- <comment></comment> -->
+                </ion-row>
+            </ion-grid>
+        </ion-content>
 </template>
 
 
