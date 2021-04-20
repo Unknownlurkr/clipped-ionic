bc30427eecc130e1026e21a7f6fbfb719df82330
Pteropticonn <ParadoxMeltdown@protonmail.com>
2021-04-19 23:17:04 -0400

    added backups

---
+++ src/backups/dataservice.js
@@ -0,0 +1,200 @@
+import { ref } from "vue";
+import  SUPABASE_CLIENT  from "./supabase-config";
+import { v4 as uuidv4 } from "uuid";
+
+const productList = ref();
+const displayError = ref();
+const session = ref();
+const commentList = ref();
+
+
+/**
+ * THIS CONNECTS TO SUPABASE...
+ */
+const dataService = () => {
+
+  const loadComments = async () => {
+    const {
+      data: comments,
+      error
+    } = await SUPABASE_CLIENT
+      .from('comments')
+      .select('*')
+      commentList.value = comments;
+      displayError.value = error;
+  }
+
+  const loadProducts = async () => {
+    const { data, error } = await SUPABASE_CLIENT.from("products").select("*");
+    productList.value = data;
+    displayError.value = error;
+  };
+
+  /*
+   * Called when dataService is first loaded to initalize the session,
+   * also sets a listener to update the session based once changes 
+   * in the auth state
+   */
+  const initialize = async () => {
+    session.value = SUPABASE_CLIENT.auth.session();
+
+    // if there is a session at startup, load the data
+    if (session.value) {
+      await loadProducts();
+    }
+
+    SUPABASE_CLIENT.auth.onAuthStateChange(async (_event, _session) => {
+      session.value = _session;
+      // if there is a session at startup, because of
+      // a state change, then load the data
+      //TODO: add conditional to check router stack
+      if (_session) {
+        await loadProducts();
+      }
+    });
+  };
+
+  //FIXME: ensure bucket is properly configured on supabase so writing to product table succeeds
+  // const saveProducts = async (formData) => {
+  //   try {
+  //     // 1) save image
+  //     const { file, ...productData } = formData;
+  //     //TODO: Get image uploading w/ user ID prefix at some point
+  //     // const imagePath = `products/${uuidv4()}-${file.name}`;
+  //     const imagePath = `products/${file.name}`;
+
+  //     //FIXME: plz
+  //     const { error: sError } = await SUPABASE_CLIENT.storage
+  //       .from("product-bucket")
+  //       .upload(imagePath, file);
+  //       debugger;
+  //       console.log(`sError ${sError}`);
+  //     if (sError){
+  //       sError.storageType = "Bucket";
+  //       throw sError;
+  //     } 
+  //     // 2) save to database with image ref
+  //     const { data: dbData, error: dbError} = await SUPABASE_CLIENT.from(
+  //       "products"
+  //     ).insert([{ ...productData, image: imagePath }]);
+  //     debugger;
+  //     console.log(`dbError ${dbError}`);
+  //     if (dbError) {
+  //       dbError.storageType = "Table";
+  //       throw dbError;
+  //     }
+
+  //     return { success: true, data: dbData };
+  //   } catch (e) {
+  //     console.log(e);
+  //     return { success: false, error: e};
+  //   }
+  // };
+
+  const saveProducts = async (formData) => {
+    try {
+      console.log({...formData.value});
+      await SUPABASE_CLIENT.from("products").insert([{...formData.value}]);
+      return { success: true };
+    } catch (error) {
+      return { success: false, error: error}
+    }
+  }
+
+  //TODO: Fix misleading naming scheme of parameter "productId" !HINT! it's not the Id, it's the entire object lol
+  const removeProduct = async (productId) => {
+    const productIdAsNumber = Number(productId.id);
+    debugger;
+    const {
+      data,
+      error
+    } = await SUPABASE_CLIENT
+      .from('products')
+      .delete()
+      .match({id: productIdAsNumber});
+
+      return { data, error };
+  };
+
+const hasUser = () => {
+  return session.value !== null;
+}
+
+const login = async (email, password) => {
+  const { user, error } = await SUPABASE_CLIENT.auth.signIn({
+    email,
+    password,
+  });
+  const userCred = SUPABASE_CLIENT.auth.user()
+  console.log(userCred);
+  console.log(error && error.message);  
+  
+  if(error == null) return { user, error };
+
+  // REVIEW: idk wth I'm doing here lol
+  // if(error.message == null) {
+  //   const newErrorMessage = "Please enter a password";
+  //   error.message = newErrorMessage;
+  // }
+
+  if (error.message == "You must provide either an email or a third-party provider.") {
+    const newErrorMessage = "You must provide an email";
+    error.message = newErrorMessage;
+  }
+
+  return { user, error };
+};
+
+const logout = async () => {
+  const { error } = await SUPABASE_CLIENT.auth.signOut();
+  console.log(error && error.message);
+  return error;
+};
+
+const createAccount = async (email, password) => {
+  const { user, error } = await SUPABASE_CLIENT.auth.signUp({
+    email,
+    password
+  });
+  console.log(error && error.message);
+  return { user, error };
+} 
+
+const forgotPassword = async (email) => {
+
+  console.log(email);
+  const { data, error } = await SUPABASE_CLIENT.auth.api.resetPasswordForEmail(email);
+  console.log(error && error.message);
+  return { data, error };
+} 
+
+const changePassword = async (password) => {
+  const { user, error } = await SUPABASE_CLIENT.auth.update({
+    password,
+    data: { passwordUpdated: new Date() }
+  })
+  console.log(error && error.message);
+  return { user, error };
+};
+
+  return {
+    // PROPERTIES
+    displayError,
+    loadProducts,
+    // FUNCTIONS
+    saveProducts,
+    productList,
+    removeProduct,
+    // Auth
+    login,
+    logout,
+    createAccount,
+    forgotPassword,
+    changePassword,
+    hasUser,
+    initialize
+  };
+};
+
+dataService().initialize();
+export default dataService;
---
+++ src/backups/router.ts
@@ -0,0 +1,159 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import Tabs from '@/views/Tabs.vue';
+import AddProduct from "@/views/AddProduct.vue";
+import Home  from "@/views/Home.vue";
+import UserLogin from "@/views/UserLogin.vue";
+import UserCreate from "@/views/UserCreate.vue";
+import ChangePassword from "@/views/ChangePassword.vue";
+import TestArea from "@/views/TestArea.vue";
+import GuestLandingPage from "@/views/GuestLandingPage.vue";
+import Welcome from "@/views/Welcome.vue";
+// import { TokenService } from "@/services/token.service";
+import  dataService  from "./dataservice.js";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/tabs/feed',
+  },
+  {
+    path: '/tabs/',
+    component: Tabs,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/feed',
+      },
+      {
+        path: 'Photos',
+        component: () => import('@/views/Photos.vue')
+      },
+      {
+        path: 'profile',
+        component: () => import('@/views/Profile.vue')
+      },
+      {
+        path: 'Feed',
+        component: () => import('@/views/Feed.vue'),
+        meta: { requiresAuth: true} 
+      },
+      {
+        path: 'Welcome',
+        component: Welcome,
+        meta: { requiresAuth: true }
+      }
+    ]
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/add-product",
+    name: "AddProduct",
+    component: AddProduct,
+    meta: { requiresAuth: true} 
+  },
+  {
+    path: "/login",
+    component: UserLogin,
+    meta: { requiresAuth: false}
+  },
+  {
+    path: "/create-account",
+     component: UserCreate,
+      meta: { requiresAuth: false} 
+  },
+  {
+    path: "/change-password",
+     component: ChangePassword,
+      meta: { requiresAuth: true} 
+  },
+
+  {
+    path: '/vids',
+    name: "Videos",
+    component: () => import('@/views/Vids.vue')
+  },
+  {
+    path: '/testarea',
+    name: "TestArea",
+    component: TestArea
+  },
+  {
+    path: '/guest-landing',
+    name: "GuestLanding",
+    component: GuestLandingPage,
+    meta: { requiresAuth: false} 
+  }
+
+  
+  // {
+  //   path: '/login',
+  //   component: SignIn,
+  //   meta: {
+  //     public: true,
+  //     onlyWhenLoggedOut: true
+  //   }
+  // },
+  // {
+  //   path: '/signup',
+  //   component: SignUp,
+  //   meta: {
+  //     public: true,
+  //     onlyWhenLoggedOut: true
+  //   }
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+    const { hasUser } = dataService();
+    console.log(`Has User:`, hasUser());
+    console.log(to.fullPath);
+    
+    //if path we're trying to go to has index of type=recover on the end...
+    if (to.fullPath.indexOf("type=recovery") != -1) {
+      next("/change-password");
+    }
+    //  else if (to.meta.requiresAuth && !hasUser()) {
+    //   next("/login");
+    // } 
+    else if (to.fullPath.includes("/tabs/feed") && !hasUser()) {      
+      next("/tabs/welcome");
+    }
+     else {
+      next();
+    }
+
+});
+
+//THIS ADDS A CHECK AT TO EACH CALL TO THE ROUTER. COMMENTING OUT BECAUSE OAUTH AUTHENTICATION IS BROKEN AF
+
+// router.beforeEach((to, from, next) => {
+//   const isPublic = to.matched.some(record => record.meta.public);
+//   const onlyWhenLoggedOut = to.matched.some(
+//     record => record.meta.onlyWhenLoggedOut
+//   );
+//   const loggedIn = !!TokenService.getToken();
+
+//   if(!isPublic && !loggedIn) {
+//     return next({
+//       path: "/login",
+//       query: { redirect: to.fullPath}
+//     });
+//   }
+//   if(loggedIn && onlyWhenLoggedOut) {
+//     return next("/tabs/tab1");
+//   }
+  
+//   next();
+// });
+
+export default router;
